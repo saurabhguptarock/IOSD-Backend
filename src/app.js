39cea45fa93a16e6c6f1bc21ec27ac47b0cdb89c
saurabh gupta <saurabhguptarock119@gmail.com>
2020-11-11 19:18:34 +0530

    updated app version

--- src/app.js
+++ src/app.js
@@ -3,8 +3,14 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv/config");
+let port = process.env.PORT | 5000;
 
 const commentsRoute = require("./routes/comments");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/comments", commentsRoute);
 
 mongoose.connect(
   process.env.DB_CONNECTION_STRING,
@@ -14,11 +20,4 @@ mongoose.connect(
   }
 );
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-app.use("/comments", commentsRoute);
-
-app.listen(process.env.PORT | 5000, () =>
-  console.log(`Listening on Port ${process.env.PORT | 5000}`)
-);
+app.listen(port, () => console.log(`Listening on Port ${port}`));
